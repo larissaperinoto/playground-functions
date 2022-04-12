8c681633cb24e5c58aef1d54bf92750e826469c2
Larissa Perinoto <perinotolarissa@gmail.com>
2022-04-12 19:02:16 +0100

    [Larissa Perinoto] - Playground Functions [Requisito 1]

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(parametro1, parametro2) {
+    if(parametro1 === true && parametro2 === true){
+      return true;
+    } else if (parametro1 === false && parametro2 === false){
+      return false;
+    } else {
+      return false;
+    }
 }
 
 // Desafio 2
