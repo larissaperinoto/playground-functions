f1b1d5451b10bc36ea2a9a16466fcce42f39a2b0
Larissa Perinoto <perinotolarissa@gmail.com>
2022-04-14 14:58:35 +0100

    [Larissa Perinoto] - Playground Functions [Requisito bônus 12]

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,28 @@
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let possibilidadeLadoAB = lineA + lineB;
+  let moduloPossibilidadeLadoAB = Math.abs(lineA - lineB);
+  let possibilidadeLadoAC = lineA + lineC;
+  let moduloPossibilidadeLadoAC = Math.abs(lineA - lineC);
+  let possibilidadeLadoBC = lineB + lineC;
+  let moduloPossibilidadeLadoBC = Math.abs(lineB - lineC);
+  let resultado = false;
+  if (lineA < possibilidadeLadoBC && lineA > moduloPossibilidadeLadoBC) {
+    resultado = true;
+  } else if (lineB < possibilidadeLadoAC && lineB > moduloPossibilidadeLadoAC) {
+    resultado = true;
+  } else if (lineC < possibilidadeLadoAB && lineC > moduloPossibilidadeLadoAB) {
+    resultado = true;
+  } else{
+    resultado = false;
+  }
+  return resultado;
 }
-
+console.log(triangleCheck(10,14,8));
 // Desafio 13
 function hydrate() {
   // seu código aqui
