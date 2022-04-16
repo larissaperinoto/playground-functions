ed26eee0356f05e179ee318a41d4ad2a9192e176
Larissa Perinoto <perinotolarissa@gmail.com>
2022-04-16 12:42:01 +0100

    [Larissa Perinoto] - Playground Functions [Requisito bônus 11]

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,57 @@
 // Desafio 11
-function generatePhoneNumber() {
+function numeroRepetido(array) {
+  let numeros = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+  }
+
+  for (let index of array) {
+    numeros[index] = numeros[index] + 1;
+  }
+  for (let index = 0; index <= 9; index += 1 ) {
+    if (numeros[index] >= 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function verificaArray(array) {
+  let verificaArray = array;
+  if (array.length !== 11){
+    verificaArray = 'Array com tamanho incorreto.';
+  } else {
+    for (let index = 0; index < array.length; index += 1) {
+      if (array[index] < 0 | array[index] > 9 | numeroRepetido(array) === false) {
+        verificaArray = 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+  return verificaArray;
+}
+
+function generatePhoneNumber(array) {
+  let arrayTelefone = '';
+  if (verificaArray(array) === array) {
+    let arrayDDD = array.slice(0,2);
+    arrayDDD = arrayDDD.join('');
+    let arrayPrimeiraParte = array.slice(2,7);
+    arrayPrimeiraParte = arrayPrimeiraParte.join('');
+    let arraySegundaParte = array.slice(7,11);
+    arraySegundaParte = arraySegundaParte.join('');
+    arrayTelefone = '(' + arrayDDD + ')' + ' ' + arrayPrimeiraParte + '-' + arraySegundaParte;
+  } else {
+    arrayTelefone = verificaArray(array);
+  }
+  return arrayTelefone;
 }
 
 // Desafio 12
@@ -44,7 +96,6 @@ function hydrate(string) {
   } else {
     stringCoposAgua = coposCerveja + " " + 'copos de água';
   }
-  
   return stringCoposAgua;
 }
 
